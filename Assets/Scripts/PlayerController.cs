fd5606a0e1fa29584cf12a13696d375f2accb3b6
modosenin0 <97416036+modosenin0@users.noreply.github.com>
2023-05-16 21:06:13 -0400

    Grandeza

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,133 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody2D theRB;
+
+    public float moveSpeed;
+    public float jumpForce;
+
+    public Transform groundPoint;
+    private bool isOnGround;
+    public LayerMask whatIsGround;
+
+    public Animator anim;
+
+    public BulletController shotToFire;
+
+    public Transform shotPoint;
+
+    private bool canDoubleJump;
+
+    public float dashSpeed, dashTime;
+
+    private float dashCounter;
+
+    public SpriteRenderer theSR, afterImage;
+    public float afterImageLifetime, timeBetweenAfterImages;
+    private float afterImageCounter;
+    public Color afterImageColor;
+
+    public float waitAfterDashing;
+    private float dashRechargeCounter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (dashRechargeCounter > 0)
+        {
+            dashRechargeCounter -= Time.deltaTime;
+        }
+        else
+        {
+            if (Input.GetButtonDown("Fire2"))
+            {
+                dashCounter = dashTime;
+
+                ShowAfterImage();
+            }
+        }
+
+
+        if (dashCounter >= 0)
+        {
+            dashCounter -= Time.deltaTime;
+            theRB.velocity = new Vector2(dashSpeed * transform.localScale.x, theRB.velocity.y);
+
+            afterImageCounter -= Time.deltaTime;
+            if (afterImageCounter <= 0)
+            {
+                ShowAfterImage();
+            }
+
+            dashRechargeCounter = waitAfterDashing;
+
+        }
+        else
+        {
+            //move sideways
+            theRB.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * moveSpeed, theRB.velocity.y);
+
+            //flip player
+            if (theRB.velocity.x < 0)
+            {
+                transform.localScale = new Vector3(-1f, 1f, 1f);
+            }
+            else if (theRB.velocity.x > 0)
+            {
+                transform.localScale = Vector3.one;
+            }
+        }
+
+        //check if player is on ground
+        isOnGround = Physics2D.OverlapCircle(groundPoint.position, 0.2f, whatIsGround);
+
+        if (Input.GetButtonDown("Jump") && (isOnGround || canDoubleJump))
+        {
+            if (isOnGround)
+            {
+                canDoubleJump = true;
+            }
+            else
+            {
+                canDoubleJump = false;
+                anim.SetTrigger("doubleJump");
+            }
+
+            theRB.velocity = new Vector2(theRB.velocity.x, jumpForce);
+        }
+
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Instantiate(shotToFire, shotPoint.position, shotPoint.rotation).moveDir = new Vector2(transform.localScale.x, 0f);
+
+            anim.SetTrigger("shotFired"); //play animation
+        }
+
+
+        anim.SetBool("isOnGround", isOnGround);
+        anim.SetFloat("speed", Mathf.Abs(theRB.velocity.x));
+    }
+
+    public void ShowAfterImage()
+    {
+        SpriteRenderer image = Instantiate(afterImage, transform.position, transform.rotation);
+        image.sprite = theSR.sprite;
+        image.transform.localScale = transform.localScale;
+        image.color = afterImageColor;
+
+        Destroy(image.gameObject, afterImageLifetime);
+
+        afterImageCounter = timeBetweenAfterImages;
+
+    }
+}
