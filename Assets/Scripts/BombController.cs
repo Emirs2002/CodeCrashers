6491944b77c98b55f57f4dba43f3a94632bfab34
modosenin0 <97416036+modosenin0@users.noreply.github.com>
2023-05-17 20:35:02 -0400

    movimientos1.0

---
+++ Assets/Scripts/BombController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombController : MonoBehaviour
+{
+    public float timeToExplode = .5f;
+    public GameObject explosionPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
