1ae2931572c96c7b792b6fa751907144463e04fc
Emily Rodriguez <em.rodriguez@correo.unimet.edu.ve>
2023-05-20 17:02:32 -0400

    arreglos camara y colores

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -6,7 +6,7 @@ public class CameraController : MonoBehaviour
 {
 
     private PlayerController player;
-    public BoxCollider2D boundBox;
+
 
     private float halfHeight, halfWidth;
 
@@ -14,8 +14,7 @@ public class CameraController : MonoBehaviour
     void Start()
     {
         player = FindObjectOfType<PlayerController>();
-        halfHeight = Camera.main.orthographicSize;
-        halfWidth = halfHeight * Camera.main.aspect;
+
     }
 
     // Update is called once per frame
@@ -23,10 +22,7 @@ public class CameraController : MonoBehaviour
     {
         if (player != null)
         {
-            transform.position = new Vector3(
-                Mathf.Clamp(player.transform.position.x, boundBox.bounds.min.x + halfWidth, boundBox.bounds.max.x - halfWidth),
-                Mathf.Clamp(player.transform.position.y, boundBox.bounds.min.y + halfHeight, boundBox.bounds.max.y - halfHeight),
-                transform.position.z);
+            transform.position = new Vector3(player.transform.position.x, player.transform.position.y, transform.position.z);
         }
     }
 }
