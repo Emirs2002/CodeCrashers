73c9138b043fb6c77a0622f8a9825e745a60049b
Emily Rodriguez <em.rodriguez@correo.unimet.edu.ve>
2023-06-24 19:55:16 -0400

    fading to black working

--- Assets/Scripts/DoorController.cs
+++ Assets/Scripts/DoorController.cs
@@ -52,10 +52,14 @@ public class DoorController : MonoBehaviour
 
         player.anim.enabled = false;
 
+        UIController.instance.StartFadeToBlack();
+
         yield return new WaitForSeconds(1.5f);
         RespawnController.instance.SetSpawn(exitPoint.position);
         player.canMove = true;
         player.anim.enabled = true;
+
+        UIController.instance.StartFadeFromBlack();
 
         SceneManager.LoadScene(levelToLoad);
     }
