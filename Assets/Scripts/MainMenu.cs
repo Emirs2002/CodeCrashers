08900d8db189762dce272e674c2e480c598bfddd
alejanndrasan <asantos@correo.unimet.edu.ve>
2023-06-30 17:05:31 -0400

    'audioManager'

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -10,7 +10,7 @@ public class MainMenu : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        AudioManager.instance.PlayMainMenuMusic();
     }
 
     public void NuevoJuego(){
